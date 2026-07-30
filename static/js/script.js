d86a355a1ca2bdcbeb589e09bf9f063325ca7423
Laveesha Dsouza <laveeshadsouza@gmail.com>
2026-07-30 23:08:07 +0530

    renamed file

--- static/js/script.js
+++ static/js/script.js
@@ -46,7 +46,7 @@ function initCountUp() {
 const STREAM_META = {
   plastic: {
     label: "Plastic",
-    image: "/static/images/pLastic.png"
+    image: "/static/images/plastic.png"
   },
   organic: {
     label: "Organic",
